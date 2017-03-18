dfa2e7d77038958b21ec32b577afcb644a76f293
yurik <josan.iurie@gmail.com>
2017-03-18 11:51:26 +0200

    Chat

---
+++ Helper/Properties/Message.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace IJChat
+{
+	[Serializable]
+	public class Message
+	{
+		public string Text;
+		public string From;
+		public string To;
+		public DateTime TimeStamp;
+
+		public Message(string text, string from, string to)
+		{
+			Text = text;
+			From = from;
+			To = to;
+		}
+	}
+}
--- IJChat/MainWindow.xaml.cs
+++ IJChat/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Helper;
 
 namespace IJChat
 {
@@ -27,7 +31,15 @@ namespace IJChat
 
 		private void button_Click(object sender, RoutedEventArgs e)
 		{
+			var text = new TextRange(rtb_two.Document.ContentStart, rtb_two.Document.ContentEnd).Text;
+			var from = "Iura";
+			var to = "Stepan";
+			var msg= new Message(text,DateTime.Now, from,to);
 
+			var tcpClient = new TcpClient(@"192.168.2.94",4444);
+			IFormatter formater = new BinaryFormatter();
+			var stream = tcpClient.GetStream();
+			formater.Serialize(stream, msg);
 		}
 	}
 }
